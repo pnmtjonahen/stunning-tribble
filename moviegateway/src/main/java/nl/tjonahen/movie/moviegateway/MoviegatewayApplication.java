40980b36dcf4a2592b240582f3620577c0afc286
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-11-01 15:44:29 +0100

    Gateway config externalized

--- moviegateway/src/main/java/nl/tjonahen/movie/moviegateway/MoviegatewayApplication.java
+++ moviegateway/src/main/java/nl/tjonahen/movie/moviegateway/MoviegatewayApplication.java
@@ -12,33 +12,33 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(UriConfiguration.class)
-//@RestController
 public class MoviegatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoviegatewayApplication.class, args);
     }
+
     
     @Bean
     public RouteLocator movieRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
         return builder.routes()
-            .route(p -> p
+                .route(p -> p
                 .path("/api/movies")
                 .uri(uriConfiguration.getMovies()))
-            .route(p -> p
+                .route(p -> p
                 .path("/api/reviews")
                 .uri(uriConfiguration.getReviews()))
-            .route(p -> p
+                .route(p -> p
                 .path("/api/watchlist")
                 .uri(uriConfiguration.getWatchlist()))
-//            .route(p -> p
-//                .host("*.hystrix.com")
-//                .filters(f -> f
-//                    .hystrix(config -> config
-//                        .setName("mycmd")
-//                        .setFallbackUri("forward:/fallback")))
-//                .uri(moviesUri))
-            .build();
+                //            .route(p -> p
+                //                .host("*.hystrix.com")
+                //                .filters(f -> f
+                //                    .hystrix(config -> config
+                //                        .setName("mycmd")
+                //                        .setFallbackUri("forward:/fallback")))
+                //                .uri(moviesUri))
+                .build();
     }
 
 //    @RequestMapping("/fallback")
@@ -46,12 +46,14 @@ public class MoviegatewayApplication {
 //        return Mono.just("fallback");
 //    }    
 }
+
 @ConfigurationProperties
 @Getter
-@Setter        
+@Setter
 class UriConfiguration {
-    private String movies = "http://localhost:8081";
-    private String reviews = "http://localhost:8082";
-    private String watchlist = "http://localhost:8083";
+
+    private String movies = "";
+    private String reviews = "";
+    private String watchlist = "";
 
 }
